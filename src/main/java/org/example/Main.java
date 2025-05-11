26d6e5473776ff19a280565ccb08e1d55edcbbdf
Christen24 <chrisfds2407@gmail.com>
2025-05-11 14:40:07 +0530

    Added the docs

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Gradle!");
+        System.out.println("This is a simple Java application.");
+        System.out.println("Gradle makes build automation easy.");
+        System.out.println("Good luck with your DevOps journey!");
+    }
+}
